3dcfdc333c0a8c9ef967eecaa3cc34c33deb2e79
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-12-13 16:05:40 -0300

    Requisito 1: cria rota / e componente Login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
