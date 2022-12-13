c82e1247fb88ad1e23a8aaf2008a3abcd8cbb45d
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-12-13 18:44:22 -0300

    Requisito 2: cria campo de input e botão que só é habilitado ao digitar 3 ou mais caracteres

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  state = {
+    isButtonDisabled: true,
+    inputName: '',
+  };
+
+  handleInput = ({ target }) => {
+    const inputValue = target.value;
+    console.log(inputValue);
+    const minLength = 3;
+
+    this.setState({
+      isButtonDisabled: (inputValue.length < minLength),
+    });
+  };
+
   render() {
+    const { isButtonDisabled } = this.state;
     return (
       <div data-testid="page-login">
         <h1>Login</h1>
+        <input
+          type="text"
+          data-testid="login-name-input"
+          onChange={ this.handleInput }
+        />
+        <button
+          disabled={ isButtonDisabled }
+          type="button"
+          data-testid="login-submit-button"
+        >
+          Entrar
+        </button>
       </div>
     );
   }
