4105c48360741c9a12bd7a99c3e98b82b490ce79
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-12-13 19:09:56 -0300

    Requisito 2: ao clicar no botão a função createUser é chamada

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,35 +1,40 @@
 import React, { Component } from 'react';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
   state = {
     isButtonDisabled: true,
-    inputName: '',
+    name: '',
   };
 
-  handleInput = ({ target }) => {
-    const inputValue = target.value;
-    console.log(inputValue);
+  handleChange = ({ target }) => {
     const minLength = 3;
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
 
     this.setState({
-      isButtonDisabled: (inputValue.length < minLength),
+      isButtonDisabled: (value.length < minLength),
+      [name]: value,
     });
   };
 
   render() {
-    const { isButtonDisabled } = this.state;
+    const { isButtonDisabled, name } = this.state;
     return (
       <div data-testid="page-login">
         <h1>Login</h1>
         <input
           type="text"
           data-testid="login-name-input"
-          onChange={ this.handleInput }
+          onChange={ this.handleChange }
+          name="name"
+          value={ name }
         />
         <button
           disabled={ isButtonDisabled }
           type="button"
           data-testid="login-submit-button"
+          onClick={ () => createUser({ name }) }
         >
           Entrar
         </button>
