f01a9cfc0e0757d9e3f668a472be4feba4bd99b1
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-12-14 16:56:59 -0300

    Requisito 3: resolvido

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react';
+import Loading from '../pages/Loading';
+import { getUser } from '../services/userAPI';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      userName: '',
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      loading: true,
+    }, async () => {
+      const user = await getUser();
+      this.setState({
+        loading: false,
+        userName: user.name,
+      });
+    });
+  }
+
   render() {
+    const { loading, userName } = this.state;
     return (
-      <header data-testid="header-component" />
+      <div>
+        {
+          loading ? <Loading />
+            : (
+              <header data-testid="header-component">
+                <p
+                  data-testid="header-user-name"
+                >
+                  {userName}
+                  Header
+                </p>
+
+              </header>
+            )
+        }
+      </div>
     );
   }
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,7 +11,6 @@ class Login extends Component {
       inputName: '',
       loading: false,
     };
-    console.log('constructor');
   }
 
   handleChange = ({ target }) => {
