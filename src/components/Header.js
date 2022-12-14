9f1c5ee1dfd63090fdc2dd81adf88e9c3e9f6917
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-12-14 17:16:01 -0300

    Requisito 4: resolvido

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Loading from '../pages/Loading';
 import { getUser } from '../services/userAPI';
 
@@ -35,9 +36,27 @@ class Header extends Component {
                   data-testid="header-user-name"
                 >
                   {userName}
-                  Header
                 </p>
+                <nav>
+                  <ul>
+                    <li>
+                      <Link to="/search" data-testid="link-to-search">
+                        Busca
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/favorites" data-testid="link-to-favorites">
+                        Favoritos
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/profile" data-testid="link-to-profile">
+                        Perfil
+                      </Link>
+                    </li>
+                  </ul>
 
+                </nav>
               </header>
             )
         }
