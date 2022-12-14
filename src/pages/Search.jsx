57ae00d11301e912c5a30d500bf629cc7f57caed
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-12-14 17:25:28 -0300

    Requisito 5: cria formulário para pesquisar artistas

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,46 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isButtonDisabled: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const minLength = 2;
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      isButtonDisabled: (value.length < minLength),
+      [name]: value,
+    });
+  };
+
   render() {
+    const { isButtonDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <h1>Search</h1>
+        <form action="">
+          <input
+            type="text"
+            name="btn"
+            id="btn-submit"
+            onChange={ this.handleChange }
+            data-testid="search-artist-input"
+          />
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ isButtonDisabled }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
