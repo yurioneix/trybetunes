76ce2a466c137962a5f752ac39b8ecbac527b88c
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-12-14 18:54:51 -0300

    Requisito 6: ao clicar em pesquisar, chama a função searchAlbumsAPI

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from './Loading';
 
 class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
       isButtonDisabled: true,
+      inputName: '',
+      loading: false,
+      resultAlbum: [],
     };
   }
 
@@ -20,28 +25,54 @@ class Search extends Component {
     });
   };
 
+  handleButton = (event) => {
+    event.preventDefault();
+
+    const { inputName } = this.state;
+
+    this.setState({
+      inputName: '',
+      loading: true,
+      artistLoading: true,
+    }, async () => {
+      const searchAlbum = await searchAlbumsAPI(inputName);
+      this.setState({
+        resultAlbum: searchAlbum,
+        loading: false,
+        artistLoading: false,
+      });
+    });
+  };
+
   render() {
-    const { isButtonDisabled } = this.state;
+    const {
+      isButtonDisabled, inputName, loading, resultAlbum, artistLoading } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <h1>Search</h1>
-        <form action="">
-          <input
-            type="text"
-            name="btn"
-            id="btn-submit"
-            onChange={ this.handleChange }
-            data-testid="search-artist-input"
-          />
-          <button
-            type="submit"
-            data-testid="search-artist-button"
-            disabled={ isButtonDisabled }
-          >
-            Pesquisar
-          </button>
-        </form>
+        { loading ? <Loading />
+          : (
+            <form action="">
+              <input
+                type="text"
+                name="inputName"
+                value={ inputName }
+                id="btn-submit"
+                onChange={ this.handleChange }
+                data-testid="search-artist-input"
+              />
+              <button
+                type="submit"
+                data-testid="search-artist-button"
+                disabled={ isButtonDisabled }
+                onClick={ this.handleButton }
+              >
+                Pesquisar
+              </button>
+            </form>
+          )}
+
       </div>
     );
   }
