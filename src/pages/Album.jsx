e90efc63fd5d1428413e794fbd68be8ee37364cb
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-12-15 15:50:33 -0300

    Requisito 7: exibe nome do artista e album ao carregar a página Álbum

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -4,16 +4,33 @@ import Header from '../components/Header';
 import getMusics from '../services/musicsAPI';
 
 class Album extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      artist: '',
+      band: '',
+    };
+  }
+
   async componentDidMount() {
     const { match: { params: { id } } } = this.props;
-    console.log(await getMusics(id));
+    const album = await getMusics(id);
+    console.log(album);
+
+    this.setState({
+      artist: album[0].artistName,
+      band: album[0].collectionName,
+    });
   }
 
   render() {
+    const { artist, band } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
         <h1>Album</h1>
+        <p data-testid="artist-name">{artist}</p>
+        <p data-testid="album-name">{band}</p>
       </div>
     );
   }
