a763201bc8f20ff8bc364ebd7bd6562024ac97ee
Yuri Rodrigues Nogueira <yurioneix18@gmail.com>
2022-12-15 21:47:01 -0300

    Requisito 8: ao clicar em favoritar a mensagem 'Carregando...' é exibida na tela e a música é favoritada

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,47 +1,60 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { addSong } from '../services/favoriteSongsAPI';
+import Loading from '../pages/Loading';
 
 class MusicCard extends Component {
   constructor(props) {
     super(props);
     this.state = {
       checkbox: false,
+      loading: false,
     };
   }
 
   handleChange = async ({ target }) => {
-    const { name } = target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
     const { songData } = this.props;
-    await addSong(songData);
 
     this.setState({
-      checkbox: value,
+      checkbox: target.checked,
+      loading: true,
+    }, async () => {
+      await addSong(songData);
+      this.setState({
+        loading: false,
+      });
     });
   };
 
   render() {
     const { previewURL, songName, trackId } = this.props;
+    const { checkbox, loading } = this.state;
     return (
-      <div>
-        <p>{songName}</p>
-        <audio data-testid="audio-component" src={ previewURL } controls>
-          <track kind="captions" />
-          O seu navegador não suporta o elemento
-          <code>audio</code>
-          .
-        </audio>
 
-        <label htmlFor="favoriteSong" data-testid={ `checkbox-music-${trackId}` }>
-          {' '}
-          Favorita
-          <input
-            type="checkbox"
-            id="favoriteSong"
-            onChange={ this.handleChange }
-          />
-        </label>
+      <div>
+        { loading ? <Loading />
+          : (
+            <>
+              <p>{songName}</p>
+              <audio data-testid="audio-component" src={ previewURL } controls>
+                <track kind="captions" />
+                O seu navegador não suporta o elemento
+                <code>audio</code>
+                .
+              </audio>
+
+              <label htmlFor="favoriteSong" data-testid={ `checkbox-music-${trackId}` }>
+                {' '}
+                Favorita
+                <input
+                  type="checkbox"
+                  id="favoriteSong"
+                  onChange={ this.handleChange }
+                  checked={ checkbox }
+                />
+              </label>
+            </>
+          )}
       </div>
     );
   }
@@ -51,6 +64,7 @@ MusicCard.propTypes = {
   previewURL: PropTypes.string.isRequired,
   songName: PropTypes.string.isRequired,
   trackId: PropTypes.number.isRequired,
+  songData: PropTypes.shape({}).isRequired,
 };
 
 export default MusicCard;
