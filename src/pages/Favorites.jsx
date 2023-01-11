ea0e1aff1fd75f15bc3a337097e7068c14bf504b
yurioneix <yurioneix18@gmail.com>
2023-01-10 22:42:41 -0300

    Requisito 12: getFavoriteSongs é chamada pra recuperar as músicas favoritas

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class Favorites extends Component {
+  state = {
+    loading: false,
+  };
+
+  async componentDidMount() {
+    await getFavoriteSongs();
+
+    this.setState({
+      loading: true,
+    });
+  }
+
   render() {
     return (
       <div data-testid="page-favorites">
